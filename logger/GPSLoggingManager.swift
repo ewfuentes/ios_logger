c99f4608fc0373f10a535c137df3939c7387c4d5
efahnestock <efahnestock@users.noreply.github.com>
2024-12-05 12:18:01 -0500

    (hopefully) fixing permission issues

--- logger/GPSLoggingManager.swift
+++ logger/GPSLoggingManager.swift
@@ -15,14 +15,33 @@ class GPSLoggingManager: NSObject, CLLocationManagerDelegate, ObservableObject {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
 //        locationManager.allowsBackgroundLocationUpdates = true // Add if background updates needed
         locationManager.pausesLocationUpdatesAutomatically = false
-        locationManager.activityType = .fitness
+//        locationManager.activityType = .fitness
+        
+        // print authorization status string
+        print("location authorization status:")
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            print("Not Determined")
+        case .restricted:
+            print("Restricted")
+        case .denied:
+            print("Denied")
+        case .authorizedAlways:
+            print("Authorized Always")
+        case .authorizedWhenInUse:
+            print("Authorized When in Use")
+        @unknown default:
+            print("Unknown")
+        }
+        print("requesting access")
         locationManager.requestWhenInUseAuthorization()
+
     }
     
     func startUpdatingLocation() {
         // Check authorization status before starting
         let authStatus = locationManager.authorizationStatus
-        guard authStatus == .authorizedWhenInUse || authStatus == .authorizedAlways else {
+        guard authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse else {
             print("Location authorization not granted")
             return
         }
@@ -33,7 +52,7 @@ class GPSLoggingManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     // Add authorization callback
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
-        case .authorizedWhenInUse, .authorizedAlways:
+        case .authorizedAlways, .authorizedWhenInUse:
             if isRecording {
                 startUpdatingLocation()
             }
